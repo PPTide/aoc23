ccab46bb3691214b1c5256aa9183da9c2e81d2c1
PPTide <52575064+PPTide@users.noreply.github.com>
2023-12-02 13:00:24 +0100

    day1 part1

--- day1.go
+++ day1.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func mainDay1(input string) string {
-	return ""
+	lines := strings.Split(input, "\n")
+	nums := make([]int, 0, len(lines))
+
+	for _, line := range lines {
+		if len(line) < 1 {
+			break
+		}
+		firstDigit := getFirstDigit(line)
+		lastDigit := getLastDigit(line)
+
+		num := firstDigit*10 + lastDigit
+		nums = append(nums, num)
+	}
+
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func getLastDigit(line string) int {
+	for i := len(line) - 1; i >= 0; i-- {
+		if line[i] >= '0' && line[i] <= '9' {
+			num, err := strconv.Atoi(string(line[i]))
+			if err != nil {
+				panic(err)
+			}
+			return num
+		}
+	}
+	panic("No last digit found for: " + line)
+}
+
+func getFirstDigit(line string) int {
+	for i := 0; i < len(line); i++ {
+		if line[i] >= '0' && line[i] <= '9' {
+			num, err := strconv.Atoi(string(line[i]))
+			if err != nil {
+				panic(err)
+			}
+			return num
+		}
+	}
+	panic("No first digit found for: " + line)
 }
