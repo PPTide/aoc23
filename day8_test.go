3c8e3c9ecd2fc4c4d333593b23ac09ed6ddbfce8
PPTide <52575064+PPTide@users.noreply.github.com>
2023-12-09 10:37:33 +0100

    day9 part1

--- day8_test.go
+++ day8_test.go
@@ -6,7 +6,7 @@ func TestMainDay8(t *testing.T) {
 	in := "LR\n\n11A = (11B, XXX)\n11B = (XXX, 11Z)\n11Z = (11B, XXX)\n22A = (22B, XXX)\n22B = (22C, 22C)\n22C = (22Z, 22Z)\n22Z = (22B, 22B)\nXXX = (XXX, XXX)"
 
 	out := mainDay8(in)
-	expectedOut := "6"
+	expectedOut := "Please find the LCM: [2 3]"
 
 	if out != expectedOut {
 		t.Fatalf("expected output \"%s\" got \"%s\"", expectedOut, out)
--- day9.go
+++ day9.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func mainDay9(in string) string {
-	return ""
+	sum := 0
+
+	for _, line := range strings.Split(in, "\n") {
+		derivatives := make([][]int, 0)
+
+		derivatives = append(derivatives, Map(strings.Split(line, " "), func(t string) int {
+			i, err := strconv.Atoi(t)
+
+			if err != nil {
+				panic(err)
+			}
+
+			return i
+		}))
+		for {
+			der, allZeros := derive(derivatives[len(derivatives)-1])
+			derivatives = append(derivatives, der)
+			if allZeros {
+				break
+			}
+		}
+		//fmt.Println(derivatives)
+		prevVal := 0
+		for i := len(derivatives) - 1; i >= 0; i-- {
+			prevVal = derivatives[i][len(derivatives[i])-1] + prevVal
+		}
+
+		sum += prevVal
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func derive(in []int) (out []int, allZeros bool) {
+	out = make([]int, 0, len(in)-1)
+
+	allZeros = true
+	for i := 1; i < len(in); i++ {
+		diff := in[i] - in[i-1]
+		out = append(out, diff)
+		if diff != 0 {
+			allZeros = false
+		}
+	}
+
+	return
 }
