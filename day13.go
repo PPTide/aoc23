3ef094aa9c5169772215c9a3d340748f447d08fa
PPTide <52575064+PPTide@users.noreply.github.com>
2023-12-13 12:07:20 +0100

    day13 prep

---
+++ day13.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func mainDay13(in string) string {
+	parts := strings.Split(in, "\n\n")
+
+	_ = parts
+
+	return strconv.Itoa(0)
+}
---
+++ day13_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestMainDay13(t *testing.T) {
+	in := "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#"
+
+	out := mainDay13(in)
+	expectedOut := "405"
+
+	if out != expectedOut {
+		t.Fatalf("expected output \"%s\" got \"%s\"", expectedOut, out)
+	}
+}
